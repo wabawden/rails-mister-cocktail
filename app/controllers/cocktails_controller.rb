f940771b1ffa62caf0d011eea0dc62aa5c352714
Bill Bawden <wabawden@aol.com>
2020-08-14 12:59:22 +0100

    Add active storage with cloudinary

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -16,7 +16,7 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
     if @cocktail.save
-        redirect_to cocktail_path(@cocktail)
+        redirect_to root_path(@cocktail)
       else
         render :new
       end
